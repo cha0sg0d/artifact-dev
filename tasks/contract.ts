f43ad7ccbaf52ae200d749b9cc52101ee115a027
cha0sg0d <tony.goss@protonmail.com>
2021-08-29 17:03:17 -0400

    query market listings

--- tasks/contract.ts
+++ tasks/contract.ts
@@ -3,13 +3,13 @@ import type { LocationId } from "@darkforest_eth/types";
 
 import { task, types } from "hardhat/config";
 import { formatEther } from "ethers/lib/utils";
-import { Signer } from "ethers";
 
 import TOKENS_ABI from "@darkforest_eth/contracts/abis/DarkForestTokens.json";
 
-import { DarkForestTokens} from "@darkforest_eth/contracts/typechain";
+import { DarkForestTokens, DarkForestTokens__factory } from "@darkforest_eth/contracts/typechain";
 import { TOKENS_CONTRACT_ADDRESS } from "@darkforest_eth/contracts";
-import { locationIdToDecStr } from "@darkforest_eth/serde";
+
+const MARKET_ADDRESS = "0x1e7cb1dbC6DaD80c86e8918382107238fb4562a8";
 
 task("artifacts", "get a player's artifacts")
 .addParam("account", "The player's address")
@@ -17,7 +17,6 @@ task("artifacts", "get a player's artifacts")
 
 async function getPlayerArtifacts(args: { account: string }, hre: HardhatRuntimeEnvironment) {
   const xDaiUrl = 'https://rpc-df.xdaichain.com/';
-  console.log('hello world')
   const provider = new hre.ethers.providers.JsonRpcProvider(xDaiUrl);
   const token = new hre.ethers.Contract(TOKENS_CONTRACT_ADDRESS, TOKENS_ABI, provider) as DarkForestTokens;
   const balance = await provider.getBalance(args.account);
@@ -37,6 +36,12 @@ task("market", "get listed artifacts")
 .setAction(getMarketArtifacts);
 
 async function getMarketArtifacts(args: { account: string }, hre: HardhatRuntimeEnvironment) {
-  
+  // load market contract address
+  const xDaiUrl = 'https://rpc-df.xdaichain.com/';
+  const provider = new hre.ethers.providers.JsonRpcProvider(xDaiUrl);
+  const tokens = DarkForestTokens__factory.connect(TOKENS_CONTRACT_ADDRESS, provider);
+  // const market = Market__factory.connect(MARKET_ADDRESS, provider);
+  const marketListings = await tokens.getPlayerArtifactIds(MARKET_ADDRESS);
+  console.log(`${marketListings.length} artifacts`);
 }
 
